82fef586353ddd74ffadd3c1aafc4b7a3b69169b
bohaohan <54bohaohan@gmail.com>
2016-03-19 00:24:04 +0800

    first commit

---
+++ test_word2vec.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+__author__ = 'bohaohan'
+from os import path
+import gensim
+
+
+_W2V_BINARY_PATH = path.dirname(path.abspath(__file__)) + "/word2vec/GoogleNews-vectors-negative300.bin"
+model = gensim.models.Word2Vec.load_word2vec_format(_W2V_BINARY_PATH, binary=True)
+london = model['中国']
+print london
+print len(london)
